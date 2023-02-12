51cd442d350a5b1abcbd02e458078dbe56bcf54a
satsuki329 <94101451+satsuki329@users.noreply.github.com>
2023-02-13 00:55:32 +0900

    リモートリポジトリでコンフリクト変更

--- PM1.cpp
+++ PM1.cpp
@@ -7,7 +7,7 @@ int main()
 {
     std::cout << "Hello VisualStudio Git!\n";
 
-    std::cout << "PullReqest branch changed\n";
+    std::cout << "Clone changed\n";
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
